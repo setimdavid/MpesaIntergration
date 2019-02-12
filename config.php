bce4d2f6307eb93f1bd2091bbf57052c25a0bbaa
David Setim <timsedee@gmail.com>
2019-02-12 16:09:16 +0300

    Hiding the ips

--- config.php
+++ config.php
@@ -18,14 +18,14 @@ class Configs
         'B2C' => array(
             'MPESAB2CSERVICEURL' => 'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest',
             'QUEUETIMEOUTURL' => 'http://5322215e.ngrok.io/MPESA_JSON/B2C/B2CQueuetimeout.php',
-            'RESULTURL' => 'https://testgateway.ekenya.co.ke:8443/MPESA_JSON/B2C/B2CResults.php',
+            'RESULTURL' => 'https://localhsot:8443/MPESA_JSON/B2C/B2CResults.php',
             'PGB2CRESULTURL' => 'http://10.20.2.28:8080/MPESAJSONService/MPESA/B2CResultsAPI'
         ),
 
         'B2C_OLD' => array(
             'MPESAB2CSERVICEURL' => 'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest',
             'QUEUETIMEOUTURL' => 'http://5322215e.ngrok.io/MPESA_JSON/B2C/B2CQueuetimeout.php',
-            'RESULTURL' => 'https://testgateway.ekenya.co.ke:8443/MPESA_JSON/B2C/B2CResults.php',
+            'RESULTURL' => 'https://localhsot:8443/MPESA_JSON/B2C/B2CResults.php',
             'PGB2CRESULTURL' => 'http://10.20.2.28:8080/MPESAJSONService/MPESA/B2CResultsAPI'
         ),
 
@@ -33,8 +33,8 @@ class Configs
             'MPESAC2BREGISTRATIONURL' => 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl',
             'MPESAC2BSIMULATIONURL' => 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate',
             'SHORTCODE' => '600000',
-            // 'VALIDATIONURL' => 'https://onlinebanking.choicemfb.com/MPESA/C2B/C2BValidation.php',
-            // 'CONFIRMATIONURL' => 'https://onlinebanking.choicemfb.com/MPESA/C2B/C2BConfirmation.php',
+            // 'VALIDATIONURL' => 'https://localhsot.com/MPESA/C2B/C2BValidation.php',
+            // 'CONFIRMATIONURL' => 'https://localhsot.choicemfb.com/MPESA/C2B/C2BConfirmation.php',
             'VALIDATIONURL' => 'http://c1899ff3.ngrok.io/MPESA/C2B/C2BValidation.php',
             'CONFIRMATIONURL' => 'http://c1899ff3.ngrok.io/MPESA/C2B/C2BConfirmation.php',
 
@@ -53,7 +53,7 @@ class Configs
         'CHECKOUT' => array(
             'CHECKOUTREQUESTURL' => 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
             'CHECKOUTQUERYURL' => 'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query',
-            'RESULTURL' => 'https://testgateway.ekenya.co.ke:8443/MPESA_JSON/CHECKOUT/CheckoutResults.php',
+            'RESULTURL' => 'https://localhsot:8443/MPESA_JSON/CHECKOUT/CheckoutResults.php',
             'PGCHECKOUTSERVICEURL' => 'http://10.20.2.28:8080/MPESAJSONService/MPESA/CheckoutResults'
         ),
         'BALANCE' => array(
@@ -74,7 +74,7 @@ class Configs
         'GENERAL' => array(
             'PUBLICCERT' => '/var/www/html-8443/MPESA_JSON/CERT/cert.cer',
             'INITIATORNAME' => 'testapi',
-            'ENCSECURITYCREDENTIALS' => 'Vl6Wgiz3hmoEvO8/YSMs/+Kn84yqpcFsjlysmNhFFUs0pG60ym0uK/VMRXEPT05p9qp0b2OD1V/7KhGOr1kgYpjeTAEuP37JkVYpKlq/+mF4g8SNeqWD80z1j0BdolWeOEZVXs/ziLE51FEK2H5FSN6eRGAL5fHBFo7Q1Adkz4E4QF0GcUAEId20axerKI+mriJWg3q5RYVFd/9au7Vu1CGl1hCO/D9evvwgOCoyc/Eb9bNc+qCb/UBLzJgFBLV6O2N9gVRmskc27UhLTZedDAt7L6NkMWXyPChDSpr7TfPKeX70nIlyxcFOiZOpBAg0e5B0LLOqfLMnn2Ftc0Lo1w=='
+            'ENCSECURITYCREDENTIALS' => 'localhsot/localhsot/+Kn84yqpcFsjlysmNhFFUs0pG60ym0uK/VMRXEPT05p9qp0b2OD1V/7KhGOr1kgYpjeTAEuP37JkVYpKlq/+mF4g8SNeqWD80z1j0BdolWeOEZVXs/ziLE51FEK2H5FSN6eRGAL5fHBFo7Q1Adkz4E4QF0GcUAEId20axerKI+mriJWg3q5RYVFd/9au7Vu1CGl1hCO/D9evvwgOCoyc/Eb9bNc+qCb/UBLzJgFBLV6O2N9gVRmskc27UhLTZedDAt7L6NkMWXyPChDSpr7TfPKeX70nIlyxcFOiZOpBAg0e5B0LLOqfLMnn2Ftc0Lo1w=='
         ),
         'SYSTEM' => array(
             'CONNECTIONTIMEOUT' => 30,
